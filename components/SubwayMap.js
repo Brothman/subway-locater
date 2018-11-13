d9f8e5273236fc0eefac88a980cd03417feed771
Ben Rothman <brothman7000@gmail.com>
2018-11-13 10:56:43 -0500

    Experimenting with mta-gtfs npm package for subway real times

--- components/SubwayMap.js
+++ components/SubwayMap.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { View, Text } from 'react-native';
 import { MapView } from 'expo';
+import axios from 'axios';
 
 const { Marker } = MapView;
  
@@ -39,6 +40,33 @@ export default class SubwayMap extends Component {
             },
         ],
     }
+  }
+
+  componentDidMount(){
+    //   const MTA_API = process.env.mta_api;
+      const MTA_API = 'dd0969df85e7f93e712dfb212544f3d9';
+    console.log('hello')
+
+      // api call to mta using isomorphic fetch
+      let getData = (req, res, next) => {
+          axios.get(`http://datamine.mta.info/mta_esi.php?key=${MTA_API}&feed_id=${req.body.field_id}`)
+              .then(fetchRes => {
+                  // console.log(fetchRes.body);
+                  var feed = GtfsRealtimeBindings.FeedMessage.decode(fetchRes.body);
+                  feed.entity.forEach(function (entity) {
+                      if (entity.trip_update) {
+                          console.log(entity.trip_update);
+                      }
+                  });
+                  console.log(feed);
+                  next();
+              }).then(jsonRes => {
+                  res.locals.mta = jsonRes;
+                  next();
+              }).catch(err => {
+                  console.log(err);
+              })
+      }
   }
 
   renderMarkers = () => {
